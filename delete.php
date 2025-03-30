2f132fa8dcd9e2c0f37d94dfd5f9c3238c5e9840
phi-xi <163747259+phi-xi@users.noreply.github.com>
2025-03-30 04:28:55 +0200

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,12 +1,12 @@
 <?php
         /*__________________________________________________
         |                                                  |
-        |                   DirbIMS                        |
-        |      Directory-based Image Management System     |
+        |                     TWIM                         |
+        |          Tiny Website Image Manager              |
         |                                                  |
-        |                  index.php                       |
+        |                  delete.php                      |
         |                                                  |
-        |    (c) PhiXi, 2025 (https://github.com/phi-xi)   |
+        |      (c) PhiXi, 2025 (github.com/phi-xi)         |
         |__________________________________________________|*/
 
 
@@ -24,29 +24,34 @@
         $_target_file   = $_TARGET_DIR . $_f;
         if ( unlink( $_target_file ) ) $_deleted_cnt++;
     }
-    if ( $_deleted_cnt > 0 ) $_msg = "$_deleted_cnt Image(s) deleted";
+    if ( $_deleted_cnt > 0 ) $_msg = "Deleting $_deleted_cnt image(s)";
 ?>
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
-        <title>IMS | Image Management System</title>
+        <title>TWIM | Tiny Website Image Manager</title>
         <link rel="icon" href="/img/icon-phixi.svg">
-        <link rel="stylesheet" href="ims.css">
-        <script type="text/javascript" src="/script/js/utils.js"></script>
+        <link rel="stylesheet" href="twim.css">
+        <script type="text/javascript" src="twim.js"></script>
     </head>
     <body>
         <div class="ims-main" style="padding-top: 30vh;">
-            <h1><?php echo($_msg);?></h1>
+            <h1>TWIM | Tiny Website Image Manager <?php echo($_VERSION);?></h1>
+            <h2><?php echo($_msg);?></h2>
             <br><br><br>
-            <div id="msg" style="text-align:left;margin-left:45vw;">Please wait...</div>
+            <h2 id="msg" style="text-align:left;margin-left:45vw;">Please wait...</h2>
         </div>
     </body>
     <script type="text/javascript">
         let msg = U.r( "msg" ),
             c = 0;
         setInterval( ()=>{
-            if ( c > 8 ) window.location.href = "./";
-            msg.innerHTML += ".";
+            if ( c > 12 ){
+                msg.innerHTML = "Done."
+            } else {
+                msg.innerHTML += ".";
+            }
+            if ( c > 16 ) window.location.href = "./";
             c++;
         }, 250 );
     </script>
